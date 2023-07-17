758fa6b38595699976d0755d1b2317d957bd39e9
Tyler Tulsky <ttulsky@gmail.com>
2023-07-17 17:15:03 +0900

    jsemial contact page update

---
+++ src/components/ContactHero.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hero-image">
+    <div class="hero-text">
+      <h1></h1>
+      <h3></h3>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {};
+</script>
+
+<style scoped>
+body,
+html {
+  height: 100%;
+}
+
+/* The hero image */
+.hero-image {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0qXzZGnaBncezIVWGKuA83taYAoiND_cdXA&usqp=CAU");
+
+  height: 50%;
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 100% auto;
+  position: relative;
+}
+
+/* Place text in the middle of the image */
+.hero-text {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+}
+@media screen and (max-width: 700px) {
+  .hero-image {
+    height: 45%; /* Adjust this value as needed */
+  }
+}
+.my-btn:hover {
+  background-color: #fc03d254 !important;
+}
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,163 @@
 <template>
-  <div class="about">
-    <h1>This is a contact page</h1>
-  </div>
+  <ContactHero />
+  <v-card class="top">
+    <div class="container">
+      <form @submit.prevent="sendEmail">
+        <strong><label>Name</label></strong>
+        <input type="text" v-model="name" name="name" placeholder="Your Name" />
+        <strong><label>Email</label></strong>
+        <input
+          type="email"
+          v-model="email"
+          name="email"
+          placeholder="Your Email"
+        />
+        <strong><label>Message</label></strong>
+        <textarea
+          name="message"
+          v-model="message"
+          cols="30"
+          rows="5"
+          placeholder="Please let me know if you have any questions or a specific project in mind!"
+        >
+        </textarea>
+
+        <input type="submit" value="Send" />
+      </form>
+    </div>
+    <h3 class="email">Axelkimsathia@gmail.com</h3>
+    <div class="contacts">
+      <v-btn href="" target=""> IG </v-btn>
+      <v-btn href="" target=""> FB </v-btn>
+    </div>
+  </v-card>
 </template>
+
+<script>
+import * as emailjs from "@emailjs/browser";
+import ContactHero from "../components/ContactHero.vue";
+
+export default {
+  name: "ContactUs",
+  data() {
+    return {
+      name: "",
+      email: "",
+      message: "",
+    };
+  },
+  components: {
+    ContactHero: ContactHero,
+  },
+  methods: {
+    async sendEmail(e) {
+      try {
+        await emailjs.sendForm(
+          process.env.VUE_APP_EMAILJS_SERVICE_ID,
+          process.env.VUE_APP_EMAILJS_TEMPLATE_ID,
+          e.target,
+          process.env.VUE_APP_EMAILJS_USER_ID,
+          {
+            name: this.name,
+            email: this.email,
+            message: this.message,
+          }
+        );
+        // If email sent successfully
+        alert(
+          "Thank you, your message has been received. We'll be in touch soon!"
+        );
+      } catch (err) {
+        if (err instanceof ReferenceError) {
+          alert("JSON Error: " + err.message);
+        } else {
+          throw err; // rethrow
+        }
+      }
+      // Reset form field
+      this.name = "";
+      this.email = "";
+      this.message = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+.email {
+  padding-top: 15px;
+  text-align: center;
+}
+
+.contacts {
+  padding-top: 15px;
+  text-align: center;
+  justify-content: space-around;
+}
+
+.contacts .v-btn {
+  margin: 0 10px;
+  color: white;
+  background-color: #fc03d2d2; /* Or any color you like */
+}
+
+.contacts .v-btn:hover {
+  background-color: #a8cff5; /* Or any color you like */
+}
+
+.top {
+  padding-top: 25px;
+  overflow-y: auto;
+  height: 100vh;
+}
+
+label {
+  float: left;
+}
+
+input[type="text"],
+[type="email"],
+textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid white;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type="submit"] {
+  background-color: #fc03d2d2;
+  color: rgb(252, 250, 250);
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type="submit"]:hover {
+  background-color: #a8cff5;
+}
+
+.container {
+  display: block;
+  margin: auto;
+  text-align: center;
+  border-radius: 5px;
+  background-color: #0c0120f7;
+  padding: 20px;
+  width: 50%;
+}
+
+/* Add media query for smaller viewports */
+@media (max-width: 700px) {
+  .container {
+    width: 90%;
+  }
+}
+</style>
